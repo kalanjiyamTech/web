861dd34f1e1d944abe752d69f5f7f4765a6a25a2
kalanjiyamTech <51898735+kalanjiyamTech@users.noreply.github.com>
2019-06-18 11:44:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 if('serviceWorker' in navigator) {
   navigator.serviceWorker
-           .register('sw.js')
+           .register('/web/sw.js')
            .then(function() { console.log('Service Worker Registered'); });
 }
 
